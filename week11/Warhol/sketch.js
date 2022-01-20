6e493f2ad2d8ae3c451df13cae7dde6b5ef425c6
Komodeo <onlythebest@gmail.com>
2022-01-20 14:23:12 -0800

    grid

--- week11/Warhol/sketch.js
+++ week11/Warhol/sketch.js
@@ -40,49 +40,62 @@ var vignette = {
   grayscale: 0
 };
 
+var grid = {
+  columns: 3,
+  rows: 3,
+  boxSize: 100
+};
+
 function setup() {
   //create canvas
-  createCanvas(100, 100);
+  createCanvas(grid.columns * grid.boxSize, grid.rows * grid.boxSize);
   background(252, 225, 237);
 }
 
 function draw() {
-  //define variables to anchor drawings
-  var centerX = width / 2;
-  var centerY = height / 2;
+  //draw a box for each grid column
+  for (var column = 0; column < grid.columns; column++) {
+    //draw a box for each grid row
+    for (var row = 0; row < grid.rows; row++) {
+      //define variables to anchor drawings
+      var centerX = grid.boxSize / 2 + column * grid.boxSize;
+      var centerY = grid.boxSize / 2 + row * grid.boxSize;
 
-  //position vulva
+      //position vulva
 
-  //majora
-  strokeWeight(0);
-  fill(vulva.majora.rgb.r, vulva.majora.rgb.g, vulva.majora.rgb.b);
-  beginShape();
-  vertex(centerX, centerY - vulva.majora.height + vulva.posY);
-  vertex(centerX + vulva.majora.width, centerY + vulva.posY);
-  vertex(centerX, centerY + vulva.majora.height + vulva.posY);
-  vertex(centerX - vulva.majora.width, centerY + vulva.posY);
-  endShape(CLOSE);
+      //majora
+      strokeWeight(0);
+      fill(vulva.majora.rgb.r, vulva.majora.rgb.g, vulva.majora.rgb.b);
+      beginShape();
+      vertex(centerX, centerY - vulva.majora.height + vulva.posY);
+      vertex(centerX + vulva.majora.width, centerY + vulva.posY);
+      vertex(centerX, centerY + vulva.majora.height + vulva.posY);
+      vertex(centerX - vulva.majora.width, centerY + vulva.posY);
+      endShape(CLOSE);
 
-  //minora
-  fill(vulva.minora.rgb.r, vulva.minora.rgb.g, vulva.minora.rgb.b);
-  beginShape();
-  vertex(centerX, centerY - vulva.minora.height + vulva.posY);
-  vertex(centerX + vulva.minora.width, centerY + vulva.posY);
-  vertex(centerX, centerY + vulva.minora.height + vulva.posY);
-  vertex(centerX - vulva.minora.width, centerY + vulva.posY);
-  endShape(CLOSE);
+      //minora
+      fill(vulva.minora.rgb.r, vulva.minora.rgb.g, vulva.minora.rgb.b);
+      beginShape();
+      vertex(centerX, centerY - vulva.minora.height + vulva.posY);
+      vertex(centerX + vulva.minora.width, centerY + vulva.posY);
+      vertex(centerX, centerY + vulva.minora.height + vulva.posY);
+      vertex(centerX - vulva.minora.width, centerY + vulva.posY);
+      endShape(CLOSE);
 
-  //penis
-  fill(penis.head.rgb.r, penis.head.rgb.g, penis.head.rgb.b);
-  ellipse(centerX, centerY + penis.head.size / 2 + penis.posY, penis.head.size);
-  fill(0);
-  ellipse(centerX, centerY + penis.tipSize / 2 + penis.posY, penis.tipSize);
+      //penis
+      fill(penis.head.rgb.r, penis.head.rgb.g, penis.head.rgb.b);
+      ellipse(centerX, centerY + penis.head.size / 2 + penis.posY, penis.head.size);
+      fill(0);
+      ellipse(centerX, centerY + penis.tipSize / 2 + penis.posY, penis.tipSize);
 
-  //vignette
-  strokeWeight(vignette.strokeSize);
-  stroke(vignette.grayscale);
-  noFill();
-  ellipse(centerX, centerY, vignette.size);
+      //vignette
+      strokeWeight(vignette.strokeSize);
+      stroke(vignette.grayscale);
+      noFill();
+      ellipse(centerX, centerY, vignette.size);
+    }
+  }
+
 }
 
 
