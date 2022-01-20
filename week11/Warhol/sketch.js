f1d161912d555746ea4ecb50cf82e0633c445c3d
Komodeo <onlythebest@gmail.com>
2022-01-19 23:26:21 -0800

    draw penis and vignette

--- week11/Warhol/sketch.js
+++ week11/Warhol/sketch.js
@@ -7,13 +7,25 @@ var vulva = {
   minora: {
     height: 10,
     width: 2
-  }
+  },
+  posY: -5
+};
+
+var penis = {
+  headSize: 60,
+  tipSize: 5,
+  posY: 0
+};
+
+var vignette = {
+  size: 130,
+  strokeSize: 50
 };
 
 function setup() {
   //create canvas
   createCanvas(100, 100);
-  background(214, 251, 255);
+  background(252, 225, 237);
 }
 
 function draw() {
@@ -21,24 +33,37 @@ function draw() {
   var centerX = width / 2;
   var centerY = height / 2;
 
+  //position vulva
+
   //majora
   strokeWeight(0);
   fill(234, 154, 183);
   beginShape();
-  vertex(centerX, centerY - vulva.majora.height);
-  vertex(centerX + vulva.majora.width, centerY);
-  vertex(centerX, centerY + vulva.majora.height);
-  vertex(centerX - vulva.majora.width, centerY);
+  vertex(centerX, centerY - vulva.majora.height + vulva.posY);
+  vertex(centerX + vulva.majora.width, centerY + vulva.posY);
+  vertex(centerX, centerY + vulva.majora.height + vulva.posY);
+  vertex(centerX - vulva.majora.width, centerY + vulva.posY);
   endShape(CLOSE);
 
   //minora
   fill(255, 135, 183);
   beginShape();
-  vertex(centerX, centerY - vulva.minora.height);
-  vertex(centerX + vulva.minora.width, centerY);
-  vertex(centerX, centerY + vulva.minora.height);
-  vertex(centerX - vulva.minora.width, centerY);
+  vertex(centerX, centerY - vulva.minora.height + vulva.posY);
+  vertex(centerX + vulva.minora.width, centerY + vulva.posY);
+  vertex(centerX, centerY + vulva.minora.height + vulva.posY);
+  vertex(centerX - vulva.minora.width, centerY + vulva.posY);
   endShape(CLOSE);
+
+  //penis
+  fill(153, 51, 47);
+  ellipse(centerX, centerY + penis.headSize / 2 + penis.posY, penis.headSize);
+  fill(0);
+  ellipse(centerX, centerY + penis.tipSize / 2 + penis.posY, penis.tipSize);
+
+  //vignette
+  strokeWeight(vignette.strokeSize);
+  noFill();
+  ellipse(centerX, centerY, vignette.size);
 }
 
 
