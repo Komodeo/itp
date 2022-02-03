98c84a65b467cbdf1a0dc1a8dcf1522876dc7373
Komodeo <onlythebest@gmail.com>
2022-02-03 15:32:21 -0800

    create background and house

--- week13/lights-on/sketch.js
+++ week13/lights-on/sketch.js
@@ -2,6 +2,22 @@ function setup() {
   //create canvas
   createCanvas(600, 390);
   background(85, 109, 124);
+
+  //draw ground
+  noStroke();
+  fill(83, 58, 84);
+  rect(0, 300, width, height - 300);
+
+  //draw wall
+  fill(250, 239, 213);
+  rect(105, 140, 390, 160);
+
+  //draw roof
+  fill(179, 50, 33);
+  triangle(100, 140, 500, 140, 300, 62);
+
+  //draw door
+  rect(280, 235, 40, 65);
 }
 
 function draw() {
