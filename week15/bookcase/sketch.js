fdc21a01ce8cbed736feb91f36eee3aaa09923f0
Komodeo <onlythebest@gmail.com>
2022-02-17 12:38:25 -0800

    initialize bookcase project and populate shelves

---
+++ week15/bookcase/sketch.js
@@ -0,0 +1,17 @@
+// populate bookcase with books
+var bookcase = [["Later", "Echo", "The Outsider", "The Great Hunt (The Wheel of Time, #2)", "A Bridge To Light , A Study in Masonic Ritual & Philosophy", "The Institute", "One Night on the Island"], //shelf 0
+["Giraffes on Horseback Salad", "The World as I See It", "Auschwitz Lullaby", "Marvel Comics: The Untold Story", "Gods of Jade and Shadow", "The Victory Garden", "In My Father's Basement", "Dark Halls", "Insatiable (Cloverleigh Farms, #3)", "Feeling Good: The New Mood Therapy", "The Corridors of Time"], //shelf 1
+["Lock In (Lock In, #1)", "Outlander (Outlander, #1)", "The Help", "Girl with a Pearl Earring", "Fever 1793", "The Miniaturist (The Miniaturist, #1)", "The Secret Life of Bees", "Jonathan Strange & Mr Norrell", "It", "Misery"], //shelf 2
+["I'm Glad You're Dead (The Preternatural Chronicles #1)", "Knight's Ransom (The First Argentines, #1)", "Scarlet Odyssey (Scarlet Odyssey, #1)", "The Queen's Poisoner (Kingfountain, #1)", "The Lord of Stariel (Stariel, #1)"], //shelf 3
+["The Novel Life Of Coral Ambrose", "How to Win Friends and Influence People", "The Zombie Survival Guide: Complete Protection from the Living Dead", "Jennifer Government", "The Haunting of Hill House", "The Island of Doctor Moreau", "1984", "The Animal Wife", "The Shining"] //shelf 4
+];
+
+function setup() {
+	createCanvas(800, 600);
+}
+
+
+
+function draw() {
+
+}
