8714d5d8eb30da8a5701e68b8e19d3ee78ca3c0e
Komodeo <onlythebest@gmail.com>
2022-02-22 19:01:15 -0800

    initialize sleuth 801-2
    and solve 801-1

--- week18/801-1-59049700-6568886/sketch.js
+++ week18/801-1-59049700-6568886/sketch.js
@@ -21,22 +21,22 @@ seats[ ][ ]
 var crowdImg;
 var spotlight;
 
-var seats = [[{"x":50,"y":50,"discovered":false},{"x":150,"y":50,"discovered":false},{"x":250,"y":50,"discovered":false},{"x":350,"y":50,"discovered":false},{"x":450,"y":50,"discovered":false},{"x":550,"y":50,"discovered":false},{"x":650,"y":50,"discovered":false},{"x":750,"y":50,"discovered":false},{"x":850,"y":50,"discovered":false},{"x":950,"y":50,"discovered":false}],[{"x":50,"y":130,"discovered":false},{"x":150,"y":130,"discovered":false},{"x":250,"y":130,"discovered":false},{"x":350,"y":130,"discovered":false},{"x":450,"y":130,"discovered":false},{"x":550,"y":130,"discovered":false},{"x":650,"y":130,"discovered":false},{"x":750,"y":130,"discovered":false},{"x":850,"y":130,"discovered":false},{"x":950,"y":130,"discovered":false}],[{"x":50,"y":210,"discovered":false},{"x":150,"y":210,"discovered":false},{"x":250,"y":210,"discovered":false},{"x":350,"y":210,"discovered":false},{"x":450,"y":210,"discovered":false},{"x":550,"y":210,"discovered":false},{"x":650,"y":210,"discovered":false},{"x":750,"y":210,"discovered":false},{"x":850,"y":210,"discovered":false},{"x":950,"y":210,"discovered":false}],[{"x":50,"y":290,"discovered":false},{"x":150,"y":290,"discovered":false},{"x":250,"y":290,"discovered":false},{"x":350,"y":290,"discovered":false},{"x":450,"y":290,"discovered":false},{"x":550,"y":290,"discovered":false},{"x":650,"y":290,"discovered":false},{"x":750,"y":290,"discovered":false},{"x":850,"y":290,"discovered":false},{"x":950,"y":290,"discovered":false}],[{"x":50,"y":370,"discovered":false},{"x":150,"y":370,"discovered":false},{"x":250,"y":370,"discovered":false},{"x":350,"y":370,"discovered":false},{"x":450,"y":370,"discovered":false},{"x":550,"y":370,"discovered":false},{"x":650,"y":370,"discovered":false},{"x":750,"y":370,"discovered":false},{"x":850,"y":370,"discovered":false},{"x":950,"y":370,"discovered":false}],[{"x":50,"y":450,"discovered":false},{"x":150,"y":450,"discovered":false},{"x":250,"y":450,"discovered":false},{"x":350,"y":450,"discovered":false},{"x":450,"y":450,"discovered":false},{"x":550,"y":450,"discovered":false},{"x":650,"y":450,"discovered":false},{"x":750,"y":450,"discovered":false},{"x":850,"y":450,"discovered":false},{"x":950,"y":450,"discovered":false}],[{"x":50,"y":530,"discovered":false},{"x":150,"y":530,"discovered":false},{"x":250,"y":530,"discovered":false},{"x":350,"y":530,"discovered":false},{"x":450,"y":530,"discovered":false},{"x":550,"y":530,"discovered":false},{"x":650,"y":530,"discovered":false},{"x":750,"y":530,"discovered":false},{"x":850,"y":530,"discovered":false},{"x":950,"y":530,"discovered":false}],[{"x":50,"y":610,"discovered":false},{"x":150,"y":610,"discovered":false},{"x":250,"y":610,"discovered":false},{"x":350,"y":610,"discovered":false},{"x":450,"y":610,"discovered":false},{"x":550,"y":610,"discovered":false},{"x":650,"y":610,"discovered":false},{"x":750,"y":610,"discovered":false},{"x":850,"y":610,"discovered":false},{"x":950,"y":610,"discovered":false}],[{"x":50,"y":690,"discovered":false},{"x":150,"y":690,"discovered":false},{"x":250,"y":690,"discovered":false},{"x":350,"y":690,"discovered":false},{"x":450,"y":690,"discovered":false},{"x":550,"y":690,"discovered":false},{"x":650,"y":690,"discovered":false},{"x":750,"y":690,"discovered":false},{"x":850,"y":690,"discovered":false},{"x":950,"y":690,"discovered":false}],[{"x":50,"y":770,"discovered":false},{"x":150,"y":770,"discovered":false},{"x":250,"y":770,"discovered":false},{"x":350,"y":770,"discovered":false},{"x":450,"y":770,"discovered":false},{"x":550,"y":770,"discovered":false},{"x":650,"y":770,"discovered":false},{"x":750,"y":770,"discovered":false},{"x":850,"y":770,"discovered":false},{"x":950,"y":770,"discovered":false}]];
+var seats = [[{ "x": 50, "y": 50, "discovered": false }, { "x": 150, "y": 50, "discovered": false }, { "x": 250, "y": 50, "discovered": false }, { "x": 350, "y": 50, "discovered": false }, { "x": 450, "y": 50, "discovered": false }, { "x": 550, "y": 50, "discovered": false }, { "x": 650, "y": 50, "discovered": false }, { "x": 750, "y": 50, "discovered": false }, { "x": 850, "y": 50, "discovered": false }, { "x": 950, "y": 50, "discovered": false }], [{ "x": 50, "y": 130, "discovered": false }, { "x": 150, "y": 130, "discovered": false }, { "x": 250, "y": 130, "discovered": false }, { "x": 350, "y": 130, "discovered": false }, { "x": 450, "y": 130, "discovered": false }, { "x": 550, "y": 130, "discovered": false }, { "x": 650, "y": 130, "discovered": false }, { "x": 750, "y": 130, "discovered": false }, { "x": 850, "y": 130, "discovered": false }, { "x": 950, "y": 130, "discovered": false }], [{ "x": 50, "y": 210, "discovered": false }, { "x": 150, "y": 210, "discovered": false }, { "x": 250, "y": 210, "discovered": false }, { "x": 350, "y": 210, "discovered": false }, { "x": 450, "y": 210, "discovered": false }, { "x": 550, "y": 210, "discovered": false }, { "x": 650, "y": 210, "discovered": false }, { "x": 750, "y": 210, "discovered": false }, { "x": 850, "y": 210, "discovered": false }, { "x": 950, "y": 210, "discovered": false }], [{ "x": 50, "y": 290, "discovered": false }, { "x": 150, "y": 290, "discovered": false }, { "x": 250, "y": 290, "discovered": false }, { "x": 350, "y": 290, "discovered": false }, { "x": 450, "y": 290, "discovered": false }, { "x": 550, "y": 290, "discovered": false }, { "x": 650, "y": 290, "discovered": false }, { "x": 750, "y": 290, "discovered": false }, { "x": 850, "y": 290, "discovered": false }, { "x": 950, "y": 290, "discovered": false }], [{ "x": 50, "y": 370, "discovered": false }, { "x": 150, "y": 370, "discovered": false }, { "x": 250, "y": 370, "discovered": false }, { "x": 350, "y": 370, "discovered": false }, { "x": 450, "y": 370, "discovered": false }, { "x": 550, "y": 370, "discovered": false }, { "x": 650, "y": 370, "discovered": false }, { "x": 750, "y": 370, "discovered": false }, { "x": 850, "y": 370, "discovered": false }, { "x": 950, "y": 370, "discovered": false }], [{ "x": 50, "y": 450, "discovered": false }, { "x": 150, "y": 450, "discovered": false }, { "x": 250, "y": 450, "discovered": false }, { "x": 350, "y": 450, "discovered": false }, { "x": 450, "y": 450, "discovered": false }, { "x": 550, "y": 450, "discovered": false }, { "x": 650, "y": 450, "discovered": false }, { "x": 750, "y": 450, "discovered": false }, { "x": 850, "y": 450, "discovered": false }, { "x": 950, "y": 450, "discovered": false }], [{ "x": 50, "y": 530, "discovered": false }, { "x": 150, "y": 530, "discovered": false }, { "x": 250, "y": 530, "discovered": false }, { "x": 350, "y": 530, "discovered": false }, { "x": 450, "y": 530, "discovered": false }, { "x": 550, "y": 530, "discovered": false }, { "x": 650, "y": 530, "discovered": false }, { "x": 750, "y": 530, "discovered": false }, { "x": 850, "y": 530, "discovered": false }, { "x": 950, "y": 530, "discovered": false }], [{ "x": 50, "y": 610, "discovered": false }, { "x": 150, "y": 610, "discovered": false }, { "x": 250, "y": 610, "discovered": false }, { "x": 350, "y": 610, "discovered": false }, { "x": 450, "y": 610, "discovered": false }, { "x": 550, "y": 610, "discovered": false }, { "x": 650, "y": 610, "discovered": false }, { "x": 750, "y": 610, "discovered": false }, { "x": 850, "y": 610, "discovered": false }, { "x": 950, "y": 610, "discovered": false }], [{ "x": 50, "y": 690, "discovered": false }, { "x": 150, "y": 690, "discovered": false }, { "x": 250, "y": 690, "discovered": false }, { "x": 350, "y": 690, "discovered": false }, { "x": 450, "y": 690, "discovered": false }, { "x": 550, "y": 690, "discovered": false }, { "x": 650, "y": 690, "discovered": false }, { "x": 750, "y": 690, "discovered": false }, { "x": 850, "y": 690, "discovered": false }, { "x": 950, "y": 690, "discovered": false }], [{ "x": 50, "y": 770, "discovered": false }, { "x": 150, "y": 770, "discovered": false }, { "x": 250, "y": 770, "discovered": false }, { "x": 350, "y": 770, "discovered": false }, { "x": 450, "y": 770, "discovered": false }, { "x": 550, "y": 770, "discovered": false }, { "x": 650, "y": 770, "discovered": false }, { "x": 750, "y": 770, "discovered": false }, { "x": 850, "y": 770, "discovered": false }, { "x": 950, "y": 770, "discovered": false }]];
 
-function setup()
-{
+function setup() {
 	createCanvas(1100, 1100);
 
 	crowdImg = loadImage("crowd.png");
 	spotlight = loadImage("spotlight.png");
 }
 
-function draw()
-{
+function draw() {
 	image(crowdImg, 0, 0);
 	////////////YOUR CODE GOES HERE////////////////////////
 
-
+	for (i = 0; i < seats.length; i++) {
+		seats[i][4].discovered = true;
+	}
 
 	///////////DON'T CHANGE ANY CODE BELOW HERE////////////
 	blendMode(BLEND);
@@ -46,17 +46,13 @@ function draw()
 	image(crowdImg, 0, 0);
 }
 
-function highlight()
-{
+function highlight() {
 	fill(255, 0, 0, 100);
 
-	for (var i = 0; i < seats.length; i++)
-	{
-		for (var j = 0; j < seats[i].length; j++)
-		{
-			if (seats[i][j].discovered)
-			{
-				image(spotlight, seats[i][j].x, seats[i][j].y, 100 , 100 );
+	for (var i = 0; i < seats.length; i++) {
+		for (var j = 0; j < seats[i].length; j++) {
+			if (seats[i][j].discovered) {
+				image(spotlight, seats[i][j].x, seats[i][j].y, 100, 100);
 			}
 		}
 	}
