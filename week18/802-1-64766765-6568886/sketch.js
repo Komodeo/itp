26e1d3e32e683161b560a8a8d753244de601eba8
Komodeo <onlythebest@gmail.com>
2022-02-22 19:54:47 -0800

    solve sleuth 802-1

--- week18/802-1-64766765-6568886/sketch.js
+++ week18/802-1-64766765-6568886/sketch.js
@@ -18,66 +18,62 @@ You aren’t going to look like much of a Poker player unless you can do a good 
 
 */
 
-var cards = [{"cardSuit":"Spades","value":"A"},{"cardSuit":"Spades","value":"2"},{"cardSuit":"Spades","value":"3"},{"cardSuit":"Spades","value":"4"},{"cardSuit":"Spades","value":"5"},{"cardSuit":"Spades","value":"6"},{"cardSuit":"Spades","value":"7"},{"cardSuit":"Spades","value":"8"},{"cardSuit":"Spades","value":"9"},{"cardSuit":"Spades","value":"10"},{"cardSuit":"Spades","value":"J"},{"cardSuit":"Spades","value":"Q"},{"cardSuit":"Spades","value":"K"},{"cardSuit":"Clubs","value":"A"},{"cardSuit":"Clubs","value":"2"},{"cardSuit":"Clubs","value":"3"},{"cardSuit":"Clubs","value":"4"},{"cardSuit":"Clubs","value":"5"},{"cardSuit":"Clubs","value":"6"},{"cardSuit":"Clubs","value":"7"},{"cardSuit":"Clubs","value":"8"},{"cardSuit":"Clubs","value":"9"},{"cardSuit":"Clubs","value":"10"},{"cardSuit":"Clubs","value":"J"},{"cardSuit":"Clubs","value":"Q"},{"cardSuit":"Clubs","value":"K"},{"cardSuit":"Hearts","value":"A"},{"cardSuit":"Hearts","value":"2"},{"cardSuit":"Hearts","value":"3"},{"cardSuit":"Hearts","value":"4"},{"cardSuit":"Hearts","value":"5"},{"cardSuit":"Hearts","value":"6"},{"cardSuit":"Hearts","value":"7"},{"cardSuit":"Hearts","value":"8"},{"cardSuit":"Hearts","value":"9"},{"cardSuit":"Hearts","value":"10"},{"cardSuit":"Hearts","value":"J"},{"cardSuit":"Hearts","value":"Q"},{"cardSuit":"Hearts","value":"K"},{"cardSuit":"Diamonds","value":"A"},{"cardSuit":"Diamonds","value":"2"},{"cardSuit":"Diamonds","value":"3"},{"cardSuit":"Diamonds","value":"4"},{"cardSuit":"Diamonds","value":"5"},{"cardSuit":"Diamonds","value":"6"},{"cardSuit":"Diamonds","value":"7"},{"cardSuit":"Diamonds","value":"8"},{"cardSuit":"Diamonds","value":"9"},{"cardSuit":"Diamonds","value":"10"},{"cardSuit":"Diamonds","value":"J"},{"cardSuit":"Diamonds","value":"Q"},{"cardSuit":"Diamonds","value":"K"}];
+var cards = [{ "cardSuit": "Spades", "value": "A" }, { "cardSuit": "Spades", "value": "2" }, { "cardSuit": "Spades", "value": "3" }, { "cardSuit": "Spades", "value": "4" }, { "cardSuit": "Spades", "value": "5" }, { "cardSuit": "Spades", "value": "6" }, { "cardSuit": "Spades", "value": "7" }, { "cardSuit": "Spades", "value": "8" }, { "cardSuit": "Spades", "value": "9" }, { "cardSuit": "Spades", "value": "10" }, { "cardSuit": "Spades", "value": "J" }, { "cardSuit": "Spades", "value": "Q" }, { "cardSuit": "Spades", "value": "K" }, { "cardSuit": "Clubs", "value": "A" }, { "cardSuit": "Clubs", "value": "2" }, { "cardSuit": "Clubs", "value": "3" }, { "cardSuit": "Clubs", "value": "4" }, { "cardSuit": "Clubs", "value": "5" }, { "cardSuit": "Clubs", "value": "6" }, { "cardSuit": "Clubs", "value": "7" }, { "cardSuit": "Clubs", "value": "8" }, { "cardSuit": "Clubs", "value": "9" }, { "cardSuit": "Clubs", "value": "10" }, { "cardSuit": "Clubs", "value": "J" }, { "cardSuit": "Clubs", "value": "Q" }, { "cardSuit": "Clubs", "value": "K" }, { "cardSuit": "Hearts", "value": "A" }, { "cardSuit": "Hearts", "value": "2" }, { "cardSuit": "Hearts", "value": "3" }, { "cardSuit": "Hearts", "value": "4" }, { "cardSuit": "Hearts", "value": "5" }, { "cardSuit": "Hearts", "value": "6" }, { "cardSuit": "Hearts", "value": "7" }, { "cardSuit": "Hearts", "value": "8" }, { "cardSuit": "Hearts", "value": "9" }, { "cardSuit": "Hearts", "value": "10" }, { "cardSuit": "Hearts", "value": "J" }, { "cardSuit": "Hearts", "value": "Q" }, { "cardSuit": "Hearts", "value": "K" }, { "cardSuit": "Diamonds", "value": "A" }, { "cardSuit": "Diamonds", "value": "2" }, { "cardSuit": "Diamonds", "value": "3" }, { "cardSuit": "Diamonds", "value": "4" }, { "cardSuit": "Diamonds", "value": "5" }, { "cardSuit": "Diamonds", "value": "6" }, { "cardSuit": "Diamonds", "value": "7" }, { "cardSuit": "Diamonds", "value": "8" }, { "cardSuit": "Diamonds", "value": "9" }, { "cardSuit": "Diamonds", "value": "10" }, { "cardSuit": "Diamonds", "value": "J" }, { "cardSuit": "Diamonds", "value": "Q" }, { "cardSuit": "Diamonds", "value": "K" }];
 var deck_img;
 var table_img;
 var drawCounter = 0;
 
-function preload()
-{
+function preload() {
 	deck_img = loadImage("deck.png");
 	table_img = loadImage("table.png");
 }
-function setup()
-{
+function setup() {
 	createCanvas(table_img.width, table_img.height);
 	frameRate(30);
 
 
 	//call your shuffleSeed function here. Followed by a call to shuffleDeck with the return value of shuffleSeed as an argument.
-
+	var seed = shuffleSeed();
+	shuffleDeck(seed);
 }
 
 //write your shuffleSeed function here
-
-
-/////////////////////DON'T CHANGE ANYTHING BELOW HERE/////////////////////////
-function shuffleDeck(shuffleSeed)
-{
-		//shuffle the deck by rotating the cards location with the values in
-		//shuffleSeed. Repeat this a random number of times between 20 and 50
-    var shuffleIterations = parseInt(random(20, 50));
-    for(var i = 0; i < shuffleIterations; i++)
-    {
-	   for (var j = 0; j < cards.length; j++)
-	   {
-		  var tempCard = cards.splice(j, 1);
-          var newLoc = (j + shuffleSeed[j])%52;
-          cards.splice(newLoc, 0, tempCard[0]);
-	   }
-    }
+function shuffleSeed() {
+	var seedArr = [];
+	for (i = 0; i < 52; i++) {
+		seedArr.push(round(random(5, 86)));
+	}
+	return seedArr;
 }
 
-function draw()
-{
+/////////////////////DON'T CHANGE ANYTHING BELOW HERE/////////////////////////
+function shuffleDeck(shuffleSeed) {
+	//shuffle the deck by rotating the cards location with the values in
+	//shuffleSeed. Repeat this a random number of times between 20 and 50
+	var shuffleIterations = parseInt(random(20, 50));
+	for (var i = 0; i < shuffleIterations; i++) {
+		for (var j = 0; j < cards.length; j++) {
+			var tempCard = cards.splice(j, 1);
+			var newLoc = (j + shuffleSeed[j]) % 52;
+			cards.splice(newLoc, 0, tempCard[0]);
+		}
+	}
+}
+
+function draw() {
 	image(table_img, 0, 0);
 
-	if (frameCount % 7 == 0)
-	{
+	if (frameCount % 7 == 0) {
 		drawCounter++;
-		if (drawCounter == 52)
-		{
+		if (drawCounter == 52) {
 			noLoop();
 		}
 	}
-	for (var i = 0; i < drawCounter; i++)
-	{
-		if (cards[i].marked)
-		{
+	for (var i = 0; i < drawCounter; i++) {
+		if (cards[i].marked) {
 			drawCard(cards[i], 400 + i * 18, 230);
 		}
-		else
-		{
+		else {
 			drawCard(cards[i], 400 + i * 18, 250);
 		}
 	}
@@ -86,19 +82,15 @@ function draw()
 }
 
 
-function drawCard(card, x, y)
-{
+function drawCard(card, x, y) {
 
 	var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 	var suits = ["Spades", "Clubs", "Hearts", "Diamonds"];
 
-	for (var i = 0; i < suits.length; i++)
-	{
-		for (var j = 0; j < values.length; j++)
-		{
-			if (card.value == values[j] && card.cardSuit == suits[i])
-			{
-			//img, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight
+	for (var i = 0; i < suits.length; i++) {
+		for (var j = 0; j < values.length; j++) {
+			if (card.value == values[j] && card.cardSuit == suits[i]) {
+				//img, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight
 				image(deck_img, j * 200, i * 300, 200, 300, x, y, 100, 150);
 				break;
 			}
