74a8d2d2081d25cc77d7138ef847ff9e7cb6a4a9
Komodeo <onlythebest@gmail.com>
2022-02-22 19:30:09 -0800

    solve sleuth 801-3

--- week18/801-3-12918189-6568886/sketch.js
+++ week18/801-3-12918189-6568886/sketch.js
@@ -22,23 +22,30 @@ You can create an additional variable if it helps.
 var crowdImg;
 var spotlight;
 
-var audience = [[{"x":50,"y":50,"located":false},{"x":150,"y":50,"located":false},{"x":250,"y":50,"located":false},{"x":350,"y":50,"located":false},{"x":450,"y":50,"located":false},{"x":550,"y":50,"located":false},{"x":650,"y":50,"located":false},{"x":750,"y":50,"located":false},{"x":850,"y":50,"located":false},{"x":950,"y":50,"located":false}],[{"x":50,"y":130,"located":false},{"x":150,"y":130,"located":false},{"x":250,"y":130,"located":false},{"x":350,"y":130,"located":false},{"x":450,"y":130,"located":false},{"x":550,"y":130,"located":false},{"x":650,"y":130,"located":false},{"x":750,"y":130,"located":false},{"x":850,"y":130,"located":false},{"x":950,"y":130,"located":false}],[{"x":50,"y":210,"located":false},{"x":150,"y":210,"located":false},{"x":250,"y":210,"located":false},{"x":350,"y":210,"located":false},{"x":450,"y":210,"located":false},{"x":550,"y":210,"located":false},{"x":650,"y":210,"located":false},{"x":750,"y":210,"located":false},{"x":850,"y":210,"located":false},{"x":950,"y":210,"located":false}],[{"x":50,"y":290,"located":false},{"x":150,"y":290,"located":false},{"x":250,"y":290,"located":false},{"x":350,"y":290,"located":false},{"x":450,"y":290,"located":false},{"x":550,"y":290,"located":false},{"x":650,"y":290,"located":false},{"x":750,"y":290,"located":false},{"x":850,"y":290,"located":false},{"x":950,"y":290,"located":false}],[{"x":50,"y":370,"located":false},{"x":150,"y":370,"located":false},{"x":250,"y":370,"located":false},{"x":350,"y":370,"located":false},{"x":450,"y":370,"located":false},{"x":550,"y":370,"located":false},{"x":650,"y":370,"located":false},{"x":750,"y":370,"located":false},{"x":850,"y":370,"located":false},{"x":950,"y":370,"located":false}],[{"x":50,"y":450,"located":false},{"x":150,"y":450,"located":false},{"x":250,"y":450,"located":false},{"x":350,"y":450,"located":false},{"x":450,"y":450,"located":false},{"x":550,"y":450,"located":false},{"x":650,"y":450,"located":false},{"x":750,"y":450,"located":false},{"x":850,"y":450,"located":false},{"x":950,"y":450,"located":false}],[{"x":50,"y":530,"located":false},{"x":150,"y":530,"located":false},{"x":250,"y":530,"located":false},{"x":350,"y":530,"located":false},{"x":450,"y":530,"located":false},{"x":550,"y":530,"located":false},{"x":650,"y":530,"located":false},{"x":750,"y":530,"located":false},{"x":850,"y":530,"located":false},{"x":950,"y":530,"located":false}],[{"x":50,"y":610,"located":false},{"x":150,"y":610,"located":false},{"x":250,"y":610,"located":false},{"x":350,"y":610,"located":false},{"x":450,"y":610,"located":false},{"x":550,"y":610,"located":false},{"x":650,"y":610,"located":false},{"x":750,"y":610,"located":false},{"x":850,"y":610,"located":false},{"x":950,"y":610,"located":false}],[{"x":50,"y":690,"located":false},{"x":150,"y":690,"located":false},{"x":250,"y":690,"located":false},{"x":350,"y":690,"located":false},{"x":450,"y":690,"located":false},{"x":550,"y":690,"located":false},{"x":650,"y":690,"located":false},{"x":750,"y":690,"located":false},{"x":850,"y":690,"located":false},{"x":950,"y":690,"located":false}],[{"x":50,"y":770,"located":false},{"x":150,"y":770,"located":false},{"x":250,"y":770,"located":false},{"x":350,"y":770,"located":false},{"x":450,"y":770,"located":false},{"x":550,"y":770,"located":false},{"x":650,"y":770,"located":false},{"x":750,"y":770,"located":false},{"x":850,"y":770,"located":false},{"x":950,"y":770,"located":false}]];
-var mobster = [false,false,false,false,false,false,false,false,false,false,false,false,false,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,false,false,false,false,false,false,false,false,false,true,false,true,false,true,false,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false];
+var audience = [[{ "x": 50, "y": 50, "located": false }, { "x": 150, "y": 50, "located": false }, { "x": 250, "y": 50, "located": false }, { "x": 350, "y": 50, "located": false }, { "x": 450, "y": 50, "located": false }, { "x": 550, "y": 50, "located": false }, { "x": 650, "y": 50, "located": false }, { "x": 750, "y": 50, "located": false }, { "x": 850, "y": 50, "located": false }, { "x": 950, "y": 50, "located": false }], [{ "x": 50, "y": 130, "located": false }, { "x": 150, "y": 130, "located": false }, { "x": 250, "y": 130, "located": false }, { "x": 350, "y": 130, "located": false }, { "x": 450, "y": 130, "located": false }, { "x": 550, "y": 130, "located": false }, { "x": 650, "y": 130, "located": false }, { "x": 750, "y": 130, "located": false }, { "x": 850, "y": 130, "located": false }, { "x": 950, "y": 130, "located": false }], [{ "x": 50, "y": 210, "located": false }, { "x": 150, "y": 210, "located": false }, { "x": 250, "y": 210, "located": false }, { "x": 350, "y": 210, "located": false }, { "x": 450, "y": 210, "located": false }, { "x": 550, "y": 210, "located": false }, { "x": 650, "y": 210, "located": false }, { "x": 750, "y": 210, "located": false }, { "x": 850, "y": 210, "located": false }, { "x": 950, "y": 210, "located": false }], [{ "x": 50, "y": 290, "located": false }, { "x": 150, "y": 290, "located": false }, { "x": 250, "y": 290, "located": false }, { "x": 350, "y": 290, "located": false }, { "x": 450, "y": 290, "located": false }, { "x": 550, "y": 290, "located": false }, { "x": 650, "y": 290, "located": false }, { "x": 750, "y": 290, "located": false }, { "x": 850, "y": 290, "located": false }, { "x": 950, "y": 290, "located": false }], [{ "x": 50, "y": 370, "located": false }, { "x": 150, "y": 370, "located": false }, { "x": 250, "y": 370, "located": false }, { "x": 350, "y": 370, "located": false }, { "x": 450, "y": 370, "located": false }, { "x": 550, "y": 370, "located": false }, { "x": 650, "y": 370, "located": false }, { "x": 750, "y": 370, "located": false }, { "x": 850, "y": 370, "located": false }, { "x": 950, "y": 370, "located": false }], [{ "x": 50, "y": 450, "located": false }, { "x": 150, "y": 450, "located": false }, { "x": 250, "y": 450, "located": false }, { "x": 350, "y": 450, "located": false }, { "x": 450, "y": 450, "located": false }, { "x": 550, "y": 450, "located": false }, { "x": 650, "y": 450, "located": false }, { "x": 750, "y": 450, "located": false }, { "x": 850, "y": 450, "located": false }, { "x": 950, "y": 450, "located": false }], [{ "x": 50, "y": 530, "located": false }, { "x": 150, "y": 530, "located": false }, { "x": 250, "y": 530, "located": false }, { "x": 350, "y": 530, "located": false }, { "x": 450, "y": 530, "located": false }, { "x": 550, "y": 530, "located": false }, { "x": 650, "y": 530, "located": false }, { "x": 750, "y": 530, "located": false }, { "x": 850, "y": 530, "located": false }, { "x": 950, "y": 530, "located": false }], [{ "x": 50, "y": 610, "located": false }, { "x": 150, "y": 610, "located": false }, { "x": 250, "y": 610, "located": false }, { "x": 350, "y": 610, "located": false }, { "x": 450, "y": 610, "located": false }, { "x": 550, "y": 610, "located": false }, { "x": 650, "y": 610, "located": false }, { "x": 750, "y": 610, "located": false }, { "x": 850, "y": 610, "located": false }, { "x": 950, "y": 610, "located": false }], [{ "x": 50, "y": 690, "located": false }, { "x": 150, "y": 690, "located": false }, { "x": 250, "y": 690, "located": false }, { "x": 350, "y": 690, "located": false }, { "x": 450, "y": 690, "located": false }, { "x": 550, "y": 690, "located": false }, { "x": 650, "y": 690, "located": false }, { "x": 750, "y": 690, "located": false }, { "x": 850, "y": 690, "located": false }, { "x": 950, "y": 690, "located": false }], [{ "x": 50, "y": 770, "located": false }, { "x": 150, "y": 770, "located": false }, { "x": 250, "y": 770, "located": false }, { "x": 350, "y": 770, "located": false }, { "x": 450, "y": 770, "located": false }, { "x": 550, "y": 770, "located": false }, { "x": 650, "y": 770, "located": false }, { "x": 750, "y": 770, "located": false }, { "x": 850, "y": 770, "located": false }, { "x": 950, "y": 770, "located": false }]];
+var mobster = [false, false, false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, true, false, true, false, true, false, true, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
 
-function setup()
-{
+function setup() {
 	createCanvas(1100, 1100);
 
 	crowdImg = loadImage("crowd.png");
 	spotlight = loadImage("spotlight.png");
 }
 
-function draw()
-{
+function draw() {
 	image(crowdImg, 0, 0);
 	////////////YOUR CODE GOES HERE////////////////////////
 
-
+	//loop through first row of audience members, compare against first ten mobster tickets
+	//then loop through second row, compare against second ten mobster tickets, and so on
+	for (row = 0; row < audience.length; row++) {
+		var startTicket = row * 10;
+		for (ticket = 0; ticket < 10; ticket++) {
+			if (mobster[startTicket + ticket] == true) {
+				audience[row][ticket].located = true;
+			}
+		}
+	}
 
 	///////////DON'T CHANGE ANY CODE BELOW HERE////////////
 	blendMode(BLEND);
@@ -48,17 +55,13 @@ function draw()
 	image(crowdImg, 0, 0);
 }
 
-function highlight()
-{
+function highlight() {
 	fill(255, 0, 0, 100);
 
-	for (var i = 0; i < audience.length; i++)
-	{
-		for (var j = 0; j < audience[i].length; j++)
-		{
-			if (audience[i][j].located)
-			{
-				image(spotlight, audience[i][j].x, audience[i][j].y, 100 , 100 );
+	for (var i = 0; i < audience.length; i++) {
+		for (var j = 0; j < audience[i].length; j++) {
+			if (audience[i][j].located) {
+				image(spotlight, audience[i][j].x, audience[i][j].y, 100, 100);
 			}
 		}
 	}
